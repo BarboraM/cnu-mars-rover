985ac31eac149ca6499108d0fee3dbf155cce3be
BarboraM <barbora.mlejnkova@gmail.com>
2017-10-21 16:01:21 +0200

    added Photo class

--- src/main/java/dk/cngroup/university/Camera.java
+++ src/main/java/dk/cngroup/university/Camera.java
@@ -3,22 +3,23 @@ package dk.cngroup.university;
 import java.util.ArrayList;
 
 public class Camera {
-    ArrayList<String> photos;
+    ArrayList<Photo> photos;
 
     public Camera() {
         photos = new ArrayList<>();
     }
 
     public void takePhoto(RoverPosition position) {
-        if (!photos.contains(position.toString())) {
-            photos.add(position.toString());
+        Photo photoToAdd = new Photo(position.getX(), position.getY());
+        if (!photos.contains(photoToAdd)) {
+            photos.add(photoToAdd);
         }
     }
 
     public void printAllPhotos() {
         System.out.println("Following coordinates contained stones:");
-        for (String photo : photos) {
-            System.out.println(photo);
+        for (Photo photo : photos) {
+            System.out.println(photo.toString());
         }
     }
 }
---
+++ src/main/java/dk/cngroup/university/Photo.java
@@ -0,0 +1,33 @@
+package dk.cngroup.university;
+
+public class Photo {
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    private int x;
+    private int y;
+
+    public Photo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return x + "," + y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Photo other = (Photo) obj;
+        return (this.getX() == other.getX()) && (this.getY() == other.getY());
+    }
+
+
+}
